57ecf1776fbd3b2ac6a3416e7302c4e987b8fd12
jieun <jangji0513@naver.com>
2024-08-09 16:45:21 +0900

    [MOD] : optional orElseThrow

--- src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
+++ src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
@@ -4,6 +4,7 @@ import com.restapi.emp.entity.Department;
 import com.restapi.emp.entity.Employee;
 import com.restapi.emp.repository.DepartmentRepository;
 import com.restapi.emp.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,15 +15,16 @@ import java.util.List;
 
 @Component
 @Profile("prod")
+@RequiredArgsConstructor
 public class EmpDepInsertRunner implements ApplicationRunner {
     final DepartmentRepository departmentRepository;
 
     final EmployeeRepository employeeRepository;
 
-    public EmpDepInsertRunner(DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
-        this.departmentRepository = departmentRepository;
-        this.employeeRepository = employeeRepository;
-    }
+//    public EmpDepInsertRunner(DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
+//        this.departmentRepository = departmentRepository;
+//        this.employeeRepository = employeeRepository;
+//    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
--- src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
@@ -7,20 +7,23 @@ import com.restapi.emp.exception.ResourceNotFoundException;
 import com.restapi.emp.repository.DepartmentRepository;
 import com.restapi.emp.service.DepartmentService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-
+@RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
     //Constructor Injection
-    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
-    }
+//    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+//        this.departmentRepository = departmentRepository;
+//    }
 
     @Override
     public DepartmentDto createDepartment(DepartmentDto departmentDto) {
@@ -31,9 +34,14 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public DepartmentDto getDepartmentById(Long departmentId) {
+//        Optional<Department> optional = departmentRepository.findById(departmentId);
+//        Department department = optional.orElseThrow(
+//                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+//        );
+        String errMsg = String.format("Department is not exists with a given id: %d",departmentId);
         Department department = departmentRepository.findById(departmentId)
                 .orElseThrow(() ->
-                        new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+                        new ResourceNotFoundException(errMsg, HttpStatus.NOT_FOUND)
         );
         return DepartmentMapper.mapToDepartmentDto(department);
     }
