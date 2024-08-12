8367e32568ca042ba60b4146b037c013c1bb003e
jieun <jangji0513@naver.com>
2024-08-12 14:57:22 +0900

    [MOD] :hibernate turnning, CorsConfig add

--- src/main/java/com/restapi/emp/BackEndRestApiApplication.java
+++ src/main/java/com/restapi/emp/BackEndRestApiApplication.java
@@ -1,7 +1,9 @@
 package com.restapi.emp;
 
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackEndRestApiApplication {
@@ -10,4 +12,11 @@ public class BackEndRestApiApplication {
 		SpringApplication.run(BackEndRestApiApplication.class, args);
 	}
 
+	//Json Datatype hibernate의 Hibernate6Module 객체를 spring bean으로 설정하고, 강제 지연로드 설정
+	@Bean
+	Hibernate6Module hibernate6Module() {
+		Hibernate6Module hibernate6Module = new Hibernate6Module();
+		hibernate6Module.configure(Hibernate6Module.Feature.FORCE_LAZY_LOADING,true);
+		return hibernate6Module;
+	}
 }
