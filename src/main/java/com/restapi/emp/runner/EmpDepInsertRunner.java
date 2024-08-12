ef2078dac1f23820c8adca0c78287d4ec5fb1c27
jieun <jangji0513@naver.com>
2024-08-12 16:04:55 +0900

    [MOD] : v2 insert sql 추가

--- src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
+++ src/main/java/com/restapi/emp/runner/EmpDepInsertRunner.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@Profile("prod")
+@Profile("local")
 @RequiredArgsConstructor
 public class EmpDepInsertRunner implements ApplicationRunner {
     final DepartmentRepository departmentRepository;
